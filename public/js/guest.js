f10eb8404f3bbcc92860fb1a787298913e2e775e
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-10-25 17:12:18 -0700

    Implemented javascript for validation

--- public/js/guest.js
+++ public/js/guest.js
@@ -12,15 +12,58 @@ document.getElementById('guest-form').onsubmit = () => {
         isValid = false;
     }
 
-
     let lname = document.getElementById('lname').value.trim();
     if (!lname) {
         document.getElementById("err-lname").style.display = "block";
         isValid = false;
     }
 
+    let met = document.getElementById('met').value.trim();
+    if (met === "select") {
+        document.getElementById("err-met").style.display = "block";
+        isValid = false;
+    }
+
+    let email = document.getElementById('email').value.trim();
+    if (email.indexOf("@") === -1 && email) {
+        document.getElementById("err-email").style.display = "block";
+        isValid = false;
+    }
+    let linkedin = document.getElementById('linkedin').value.trim();
+    if (!linkedin.includes("https://linkedin.com/in/")  && linkedin) {
+        document.getElementById("err-linkedin").style.display = "block";
+        isValid = false;
+    }
+
+    let checkBox = document.getElementById('added');
+    if (checkBox.checked && !email) {
+        document.getElementById("err-email2").style.display = "block";
+        document.getElementById("bottom").style.display = "block";
+        isValid = false;
+    }
+    
+    
+
     return isValid;
 }
+let checkBox = document.getElementById('added');
+checkBox.addEventListener('change', () =>{
+    if(checkBox.checked){
+        document.getElementById('bottom').style.display = "block";
+    } else{
+        document.getElementById('bottom').style.display = "none";
+    }
+    
+});
+let bottom = document.getElementById('bottom');
+document.getElementById('met').addEventListener("change", ()=>{
+    let other = document.getElementById("second-section-right");
+    if(document.getElementById('met').value.trim() === "other"){
+        other.style.display = "block";
+    } else{
+        other.style.display = "none";
+    }
+});
 
 function clearErrors() {
     let errors = document.getElementsByClassName("error");
