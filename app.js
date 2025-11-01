4d3eb4b49ef3ab8bc4d52cf0263f99fa4e313c5c
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-10-31 18:05:50 -0700

    Created the admin page to view submissions.

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 3004;
+
+const submissions = [];
 
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
 
-app.post('/submit-form', (req,res) => {
+app.get('/admin', (req, res) =>{
+    res.send(submissions);
+});
+
+app.post('/submit-form', (req, res) => {
+    const submission = req.body;
+    submissions.push(submission);
+    console.log(submissions);
+    
     res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
 });
 
