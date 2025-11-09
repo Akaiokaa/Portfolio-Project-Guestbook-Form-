c09bd454e894ef9cf62a1db19f64d56e55d752b8
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-11-08 18:59:25 -0800

    Added NA values if there is no input.

--- app.js
+++ app.js
@@ -27,6 +27,25 @@ app.post('/submit-form', (req, res) => {
     let date = new Date();
     const submission = req.body;
     submission.submissionTime = date.toLocaleString();
+
+    if (!submission.jobT || submission.jobT.trim() === "") {
+        submission.jobT = "NA";
+    }
+    if (!submission.company || submission.company.trim() === "") {
+        submission.company = "NA";
+    }
+    if (!submission.linkedin || submission.linkedin.trim() === "") {
+    submission.linkedin = "NA";
+    }
+    if (!submission.email || submission.email.trim() === "") {
+        submission.email = "NA";
+    }
+    if (!submission.message || submission.message.trim() === "") {
+        submission.message = "NA";
+    }
+    if (!submission.choiceFormat || submission.choiceFormat.trim() === "") {
+        submission.choiceFormat = "NA";
+    }
     submissions.push(submission);
     console.log(submissions);
     
